1c204dab84867b48ac8e08143999f8a31c56df1f
sampletext32 <sampletext32@bk.ru>
2020-04-30 20:31:01 +0300

    Add project

---
+++ Petrov_3/main.cpp
@@ -0,0 +1,36 @@
+﻿// .cpp : main project file.
+// Программа 
+// Автор: 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	cout << "Автор: Петров Александр Константинович\n";
+	cout << "Программа для расчёта срока обучения\n";
+
+	int n1, n2, m, n;
+	cout << "N1: ";
+	cin >> n1;
+	cout << "N2: ";
+	cin >> n2;
+	cout << "M: ";
+	cin >> m;
+	cout << "N: ";
+	cin >> n;
+
+	while (n1 <= n2)
+	{
+		if (n1 % m == 0 && n1 % n != 0)
+		{
+			cout << n1 << "\n";
+		}
+		n1++;
+	}
+
+	system("pause");
+	return 0;
+}
